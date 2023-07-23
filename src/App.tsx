f9f0fd33a1b78aae1ef06de41b3b1ec64e2edd5e
1Molot <sinkevich1990@gmail.com>
2023-07-23 18:30:46 +0300

    fix:add-styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+// import { Link } from 'react-router-dom'
+//
+// export function App() {
+//   return (
+//       <div>
+//         <Link to={'/home'}>
+//           <button>Go home</button>
+//         </Link>
+//         <Link to={'/home'}>Go home</Link>
+//       </div>
+//   )
+// }
 export function App() {
   return <div>Hello</div>
 }
