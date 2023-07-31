e19725a4027b049b9ca664d126492dba580494db
1Molot <sinkevich1990@gmail.com>
2023-07-31 18:00:22 +0300

    fix:login-form

--- src/components/auth/login-form/login-form.tsx
+++ src/components/auth/login-form/login-form.tsx
@@ -1,68 +1,55 @@
-// import { DevTool } from '@hookform/devtools'
-// import { useController, useForm } from 'react-hook-form'
-// import { z } from 'zod'
-//
-// //import { TextField, Button, Checkbox } from '@/components'
-//
-// type FormValues = {
-//   email: string
-//   password: string
-//   rememberMe: boolean
+import { DevTool } from '@hookform/devtools'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 
-// }
-// const emailRegex =
-//   /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
-//
-// const MIN_PASSWORD_LENGTH = 3
-//
-// const loginSchema = z.object({
-//   email: z.string().email(),
-//   password: z.string().min(MIN_PASSWORD_LENGTH),
-//   remembreMe: z.boolean().default(false),
-// })
-//
-// export const LoginForm = () => {
-//   const { register, handleSubmit, control } = useForm<FormValues>({
-//     resolver: zedResolve(loginSchema),
-//   })
-//
-//   const {
-//     field: { value, onChange },
-//     formState: { errors },
-//   } = useController({
-//     name: 'rememberMe',
-//     control,
-//     defaultValue: false,
-//   })
-//
-//   console.log(errors)
-//
-//   const onSubmit = (data: FormValues) => {
-//     console.log(data)
-//   }
-//
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <DevTool control={control} />
-//       <TextField
-//         {...register('email', {
-//           required: 'Email is required',
-//           pattern: { value: emailRegex, message: 'Invalid email' },
-//         })}
-//         label={'email'}
-//       />
-//       <TextField
-//         {...register('password', {
-//           required: 'Password is required',
-//           minLength: {
-//             value: MIN_PASSWORD_LENGTH,
-//             message: `Password must be at least ${MIN_PASSWORD_LENGTH} characters`,
-//           },
-//         })}
-//         label={'password'}
-//       />
-//       <Checkbox checked={value} onValueChange={onChange} label={'remember me'} />
-//       <Button option="primer" type="submit">Submit</Button>
-//     </form>
-//   )
-// }
+import { Button } from '../../ui/button'
+import { ControlledCheckbox } from '../../ui/controlled'
+import { TextField } from '../../ui/text-field'
+
+const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(3),
+  rememberMe: z.boolean().default(false),
+})
+
+type FormValues = z.infer<typeof loginSchema>
+
+export const LoginForm = () => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(loginSchema),
+  })
+  const onSubmit = (data: FormValues) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <DevTool control={control} />
+      <TextField
+        {...register('email')}
+        errorMessage={errors.email?.message}
+        label={'email'}
+        type={'default'}
+      />
+      <TextField
+        {...register('password')}
+        errorMessage={errors.password?.message}
+        label={'password'}
+        type={'password'}
+      />
+      <ControlledCheckbox
+        variant={'withText'}
+        checkBoxText={'remember me'}
+        control={control}
+        name={'rememberMe'}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
