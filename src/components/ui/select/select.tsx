af8f9c69d04378b5ffbdb785192d88091bdac191
1Molot <sinkevich1990@gmail.com>
2023-07-31 22:41:06 +0300

    fix:select

---
+++ src/components/ui/select/select.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react'
+
+import { Listbox } from '@headlessui/react'
+
+import { SelectArrow } from '../../../assets/icons'
+import { Typography } from '../typography'
+
+import s from './select.module.scss'
+
+type PropsType = {
+  options?: any[]
+  onChangeOption?: (option: any) => void
+  className?: string
+  isDisabled?: boolean
+  value: any
+}
+
+export const SelectDemo: FC<PropsType> = ({ isDisabled, onChangeOption, options, value }) => {
+  return (
+    <Listbox disabled={isDisabled} value={value} onChange={onChangeOption}>
+      <div className={s.listBox}>
+        <Listbox.Button className={isDisabled ? s.disabledTrigger : s.trigger}>
+          <Typography variant={'Body1'}>{value.value}</Typography>
+          <SelectArrow className={isDisabled ? s.disabledArrow : s.arrow} />
+        </Listbox.Button>
+        <Listbox.Options className={s.optionList}>
+          {options?.map(o => (
+            <Listbox.Option className={s.list} key={o.id} value={o}>
+              {({ disabled }) => (
+                <Typography className={`${disabled ? s.listItemDisabled : s.listItem}`}>
+                  {o.value}
+                </Typography>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </div>
+    </Listbox>
+  )
+}
