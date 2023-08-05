d49347292d61af78e5a07fb162991cc998c9f04e
1Molot <sinkevich1990@gmail.com>
2023-08-05 15:55:46 +0300

    fix:fix-and-popytka-sdelat-rout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { RouterAuth } from './components/auth/route-auth/route-auth.tsx'
 import { Button } from './components/ui/button'
 import { Header } from './components/ui/header/header.tsx'
 import { Modal } from './components/ui/modal'
@@ -10,6 +11,7 @@ export function App() {
       <Typography variant="H1">Maksim</Typography>
       <Header isAuth={true} />
       <Modal open={true} />
+      <RouterAuth />
     </div>
   )
 }
